205e8f097660e370f8c87f5d964a51d18011b5d9
Santiago Libano <santilibano27@gmail.com>
2023-05-24 14:59:26 -0300

    Fix: Bug que hacía que la API no pase el test

--- test/api.test.js
+++ test/api.test.js
@@ -10,13 +10,13 @@ describe("API substract", () => {
     test("Deberia responder con un 200 ok", async () => {
         const app = await api.build()
 
-        request(app).get('/api/v1/sub/2/1')
+        return request(app)
+            .get('/api/v1/sub/2/1')
             .expect(200)
             .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err
-
+            .then((res) => {
                 expect(res.body.result).toEqual(1);
-            })
-    })
-})
+            });
+    });
+});
+
