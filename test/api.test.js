1a34d9c8811ca755fae774a5334df96de939a6ac
Santiago Libano <santilibano27@gmail.com>
2023-05-24 15:03:24 -0300

    Feature: API add y tests pertinentes

--- test/api.test.js
+++ test/api.test.js
@@ -20,3 +20,30 @@ describe("API substract", () => {
     });
 });
 
+describe("API add", () => {
+    test("Debería responder con un 200 ok", async () => {
+        const app = await api.build();
+
+        return request(app)
+            .get('/api/v1/add/2/1')
+            .expect(200)
+            .expect("Content-Type", "application/json; charset=utf-8")
+            .then((res) => {
+                expect(res.body.result).toEqual(3);
+            });
+    });
+
+    //En el endpoint de suma, probar que si el segundo parámetro es negativo, 
+    //el resultado sea menor al primer parámetro y que el endpoint devuelva un status 200.
+    test("Debería responder con un 200 ok y el resultado menor al primer parámetro si el segundo parámetro es negativo", async () => {
+        const app = await api.build();
+
+        return request(app)
+            .get('/api/v1/add/5/-3')
+            .expect(200)
+            .expect("Content-Type", "application/json; charset=utf-8")
+            .then((res) => {
+                expect(res.body.result).toBeLessThan(5);
+            });
+    });
+});
