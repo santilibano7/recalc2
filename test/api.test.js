e99ad701dbdd6e606da3b02ec9be5f0add458aa9
kralsito <franco_kral@hotmail.com>
2023-05-28 17:48:42 -0300

    Feature: Test de multiplicacion con decimales

--- test/api.test.js
+++ test/api.test.js
@@ -47,3 +47,33 @@ describe("API add", () => {
             });
     });
 });
+
+    //En el endpoint de multiplicación, probar que si ambos parámetros tienen decimales,
+    //el resultado sea con decimales y que el endpoint devuelva un status 200.
+    
+    describe("API mul", () => {
+        test("Debería responder con un 200 ok", async () => {
+            const app = await api.build();
+    
+            return request(app)
+                .get('/api/v1/mul/2/1')
+                .expect(200)
+                .expect("Content-Type", "application/json; charset=utf-8")
+                .then((res) => {
+                    expect(res.body.result).toEqual(2);
+                });
+        });
+
+        test("Debería responder con un 200 ok y si el resultado da decimales, teniendo ambos parametros con decimales", async () => {
+            const app = await api.build();
+    
+            return request(app)
+                .get('/api/v1/mul/1.5/2.5')
+                .expect(200)
+                .expect("Content-Type", "application/json; charset=utf-8")
+                .then((res) => {
+                    expect(res.body.result).toEqual(3.75);
+                });
+        });
+});
+    
